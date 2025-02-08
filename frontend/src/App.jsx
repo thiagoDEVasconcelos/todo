05a288e1774e458245b170d0ba8e32087853b795
thiagoDEVasconcelos <thiagovsousa21@gmail.com>
2025-02-08 02:28:25 -0300

    Adicionando a funcionalidade de handleEdit e handleDelete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,46 @@
-import './App.css'
+import { useState } from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-import { useState } from 'react';
+import './App.css'
 
 function App() {
   const [tarefa, setTarefa] = useState("");
-  const [listaDeTarefas, setListaDeTarefas] = useState([
-    "Levar meu cachorrinho para passear."
-  ]);
+  const [listaDeTarefas, setListaDeTarefas] = useState([]);
+  const [edicaoIndex, setEdicaoIndex] = useState(null);
 
   const handleChange = (e) => {
-    setTarefa(e.target.value);
+    setTarefa(e.target.value);    
   }
   const handleSubmit = (e) => {
     e.preventDefault();
     e.stopPropagation();
+    
     if (tarefa.trim() === "") return;
+    
+    if (edicaoIndex !== null) {
+      const novasTarefas = [...listaDeTarefas];
+      novasTarefas[edicaoIndex] = tarefa.trim();
+      setListaDeTarefas(novasTarefas);
+      setTarefa("");
+      setEdicaoIndex(null); 
+    } else {
     const novaTarefa = tarefa.trim();
     setListaDeTarefas([...listaDeTarefas, novaTarefa]);
     setTarefa("");
+    }
+    
   }
-
+  const handleEdit = (e, index)=> {
+    setTarefa(listaDeTarefas[index])
+    setEdicaoIndex(index);
+  }
+  const handleDelete = (e, index) => {
+    e.preventDefault();
+    const novasTarefas = [...listaDeTarefas];
+    novasTarefas.splice(index, 1);
+    setListaDeTarefas([...novasTarefas])
+  }
 
   return (
     <>
@@ -41,8 +60,8 @@ function App() {
             {listaDeTarefas.map((tarefa, index) => {
               return <div key={index} className='list-item'>
               <li key={index}>{tarefa}</li>
-              <EditIcon sx={{cursor: "pointer"}}/>
-              <DeleteIcon sx={{cursor: "pointer"}}/>
+              <EditIcon sx={{cursor: "pointer"}} onClick={(e) => handleEdit(e, index)}/>
+              <DeleteIcon sx={{cursor: "pointer"}} onClick={e => handleDelete(e, index)}/>
               </div>
             })}
           </ul>
